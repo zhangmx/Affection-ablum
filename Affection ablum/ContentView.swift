a50d0ca8b3b124ee4bb2e3b950b8784d43f014c2
张明星 <icediv@163.com>
2020-07-22 18:31:12 +0800

    Initial Commit

---
+++ Affection ablum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Affection ablum
+//
+//  Created by 张明星 on 2020/7/22.
+//  Copyright © 2020 张明星. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Affection ablumTests/Affection_ablumTests.swift
@@ -0,0 +1,34 @@
+//
+//  Affection_ablumTests.swift
+//  Affection ablumTests
+//
+//  Created by 张明星 on 2020/7/22.
+//  Copyright © 2020 张明星. All rights reserved.
+//
+
+import XCTest
+@testable import Affection_ablum
+
+class Affection_ablumTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
