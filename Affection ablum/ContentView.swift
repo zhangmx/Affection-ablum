b3650f4729f9ea1d09424dcd4ef88001e40ddcf9
张明星 <icediv@163.com>
2020-07-28 19:20:24 +0800

    finish first step

--- Affection ablum/ContentView.swift
+++ Affection ablum/ContentView.swift
@@ -11,19 +11,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height:300)
             
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
         
     }
 }
